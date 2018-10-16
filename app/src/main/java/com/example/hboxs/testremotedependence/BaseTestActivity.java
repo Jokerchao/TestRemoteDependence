9c921879da8597e1ad0c08c7261fd4a0d9bf8801
chaos <chaos@hboxs.com>
2018-10-16 16:01:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hboxs/testremotedependence/BaseTestActivity.java
@@ -0,0 +1,11 @@
+package com.example.hboxs.testremotedependence;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chaos
+ * on 2018/10/16
+ */
+public class BaseTestActivity extends AppCompatActivity {
+}
