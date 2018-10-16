4e870acb2ce4e61b5955b4c8883666f99b164865
chaos <chaos@hboxs.com>
2018-10-16 16:23:05 +0800

    test

--- app/src/main/java/com/example/hboxs/testremotedependence/MainActivity.java
+++ app/src/main/java/com/example/hboxs/testremotedependence/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.hboxs.testremotedependence;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.hboxs.testremotelibrary.HelloUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivityHello";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String hello = HelloUtil.getHello();
+        Log.d(TAG,hello);
     }
 }
---
+++ testremotelibrary/src/main/java/com/example/hboxs/testremotelibrary/HelloUtil.java
@@ -0,0 +1,11 @@
+package com.example.hboxs.testremotelibrary;
+
+/**
+ * Created by Chaos
+ * on 2018/10/16
+ */
+public class HelloUtil {
+    public static String getHello(){
+        return "Hello,my world!";
+    }
+}
